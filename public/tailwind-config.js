d80918880bfab4b3662f2fce17747c882b91d3b8
Ktune-kpop <kevincaonav@gmail.com>
2026-04-13 22:42:57 +0700

    update

---
+++ public/tailwind-config.js
@@ -0,0 +1,16 @@
+﻿tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'primary-sepia': '#F5F5DC',
+                        'secondary-sepia': '#A0522D',
+                        'highlight': '#654321',
+                        'accent': '#583a26',
+                    },
+                    fontFamily: {
+                        'serif-title': ['"Playfair Display"', 'serif'],
+                        'sans-content': ['Montserrat', 'sans-serif'],
+                    }
+                }
+            }
+        }
